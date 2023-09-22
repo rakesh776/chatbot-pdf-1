3dfa8c2639b471a7281e75fd0281bd1d6a928004
rakeshkumawat12 <rakeshkumawatrrkk12877@gmail.com>
2023-09-22 23:11:34 +0530

    updated

--- hello.py
+++ hello.py
@@ -2,11 +2,6 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-# from langchain.embeddings import OpenAIEmbeddings
-# from langchain.vectorstores import FAISS
-# from langchain.chat_models import ChatOpenAI
-# from langchain.memory import ConversationBufferMemory
-# from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import openai
